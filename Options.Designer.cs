0af73326c33dafc06562af17431d31fab368569a
GarciaMauricio <mg5757540@gmail.com>
2022-06-09 15:00:47 -0500

    Seed Dialog Box & Options Dialog Box

--- Options.Designer.cs
+++ Options.Designer.cs
@@ -29,13 +29,37 @@ namespace MauricioGarcia_GameOfLife
         /// </summary>
         private void InitializeComponent()
         {
+            this.Cancel = new System.Windows.Forms.Button();
+            this.Ok = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // Cancel
+            // 
+            this.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Cancel.Location = new System.Drawing.Point(187, 160);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(75, 23);
+            this.Cancel.TabIndex = 3;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            // 
+            // Ok
+            // 
+            this.Ok.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.Ok.Location = new System.Drawing.Point(97, 160);
+            this.Ok.Name = "Ok";
+            this.Ok.Size = new System.Drawing.Size(75, 23);
+            this.Ok.TabIndex = 2;
+            this.Ok.Text = "Ok";
+            this.Ok.UseVisualStyleBackColor = true;
             // 
             // Options
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(369, 195);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.Ok);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -46,5 +70,8 @@ namespace MauricioGarcia_GameOfLife
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.Button Ok;
     }
 }
--- Seed.Designer.cs
+++ Seed.Designer.cs
@@ -29,13 +29,37 @@ namespace MauricioGarcia_GameOfLife
         /// </summary>
         private void InitializeComponent()
         {
+            this.Ok = new System.Windows.Forms.Button();
+            this.Cancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // Ok
+            // 
+            this.Ok.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.Ok.Location = new System.Drawing.Point(99, 160);
+            this.Ok.Name = "Ok";
+            this.Ok.Size = new System.Drawing.Size(75, 23);
+            this.Ok.TabIndex = 0;
+            this.Ok.Text = "Ok";
+            this.Ok.UseVisualStyleBackColor = true;
+            // 
+            // Cancel
+            // 
+            this.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Cancel.Location = new System.Drawing.Point(189, 160);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(75, 23);
+            this.Cancel.TabIndex = 1;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
             // 
             // Seed
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(369, 195);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.Ok);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -46,5 +70,8 @@ namespace MauricioGarcia_GameOfLife
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Ok;
+        private System.Windows.Forms.Button Cancel;
     }
 }
